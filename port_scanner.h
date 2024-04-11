9511b8ae989b8f5fc4f5d9b2465c034df38a9c13
KiPTM <133209150+KiPTM@users.noreply.github.com>
2024-04-11 15:43:51 +0300

    Update port_scanner.h

--- port_scanner.h
+++ port_scanner.h
@@ -1,12 +1,12 @@
 #ifndef PORT_SCANNER_H
 #define PORT_SCANNER_H
 
-// Function to parse command line arguments
+/* parse command line arguments */
 int parse_arguments(int argc, char *argv[], char *target_ip, int *start_port, int *end_port);
 
-// Function to scan ports
+/* scan ports */
 void scan_ports(char *target_ip, int start_port, int end_port);
 
 void scan_ports_concurrent(char *target_ip, int start_port, int end_port, int num_threads);
 
-#endif // PORT_SCANNER_H
+#endif /* PORT_SCANNER_H */
